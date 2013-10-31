cdbd1a656565060b360bb69010df3a9c14969284
Yang Sheng <yangsheng6810@gmail.com>
2013-10-31 16:38:10 +0800

    Add thread safety

--- output.cpp
+++ output.cpp
@@ -1,18 +1,29 @@
 #include <fstream>
 #include "output.h"
+// using namespace boost::gil;
 
 Output::Output(int ww, int hh):
     width(ww), height(hh),
     image(new array_type(boost::extents[height][width]))
 {
+    // img = rgb8_image_t(ww, hh);
+    // v = view(img);
+	// rgb8_pixel_t black(0, 0, 0);
+    // fill_pixels(view(img), black);
+	Color c("black");
+	for (int i = 0; i < width; ++i)
+		for(int j = 0; j < height; ++j)
+	        (*image)[j][i] = c;
+
 }
 
 Output::~Output()
 {
 }
 
-void Output::writePic()
+void Output::writePic() const
 {
+    boost::mutex::scoped_lock lock(mutex_output);
 	std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
 	ofs << "P6\n" << width << " " << height << "\n255\n";
 	for (unsigned i = 0; i < height; ++i)
@@ -22,10 +33,13 @@ void Output::writePic()
 		    (unsigned char)((*image)[i][j].b * 255);
 	    }
 	ofs.close();
-
+	// png_write_view("untitled.png", const_view(img));
 }
 
 void Output::addColor(Color c, int x, int y)
 {
+    boost::mutex::scoped_lock lock(mutex_modify);
+	c.toRealColor();
 	(*image)[y][x] = c.toRealColor();
+	// v(y, x) = rgb8_pixel_t(c.r, c.g, c.b);
 }
--- output.h
+++ output.h
@@ -1,11 +1,16 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 #include "color.h"
+#define png_infopp_NULL (png_infopp)NULL
+#define int_p_NULL (int*)NULL
 #include <boost/multi_array.hpp>
 #include <boost/smart_ptr.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
+// #include <boost/gil/gil_all.hpp>
+// #include <boost/gil/extension/io/png_dynamic_io.hpp>
 
 typedef boost::multi_array<Color, 2> array_type;
-typedef array_type::index index;
 
 class Output
 {
@@ -13,9 +18,16 @@ public:
 	Output(int ww = 800, int hh = 600);
 	~Output();
 	void addColor(Color c, int x, int y);
-	void writePic();
+	void writePic() const;
 	int width, height;
+
+private:
 	boost::scoped_ptr<array_type> image;
+	mutable boost::mutex mutex_output;
+	mutable boost::mutex mutex_modify;
+	// boost::gil::rgb8_image_t img;
+	// boost::gil::rgb8_view_t v;
+
 };
 
 #endif // OUTPUT_H
