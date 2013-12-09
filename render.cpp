93ee44451276a1d0092664c8afc9ecbf168b7eb0
Yang Sheng <yangsheng6810@gmail.com>
2013-12-09 20:13:00 +0800

    update python intergation, add ``object addtion''.

--- render.cpp
+++ render.cpp
@@ -1,13 +1,20 @@
+#include <iostream>
 #include <string>
 #include "scene.h"
 #include "tracer.h"
 #include "vector3.h"
 #include "point3.h"
+#include "color.h"
+#include <vector>
 #include <boost/smart_ptr.hpp>
 #include <boost/python.hpp>
 using namespace boost::python;
 
 boost::shared_ptr<Scene> scene;
+PyObject *renderEngine;
+PyObject *pythonCallback;
+PyObject *pythonAddTile;
+int total_sample = 0;
 
 char const* yay()
 {
@@ -21,9 +28,101 @@ char const* add_objects(object s)
     return sstr;
 }
 
-char const* new_scene(void)
+void add_object()
+{
+	scene->addObject();
+}
+
+void add_vertice(object point, object normal)
+{
+	Point3 c_point = Point3(extract<double>(point.attr("x")),
+	                        extract<double>(point.attr("y")),
+	                        extract<double>(point.attr("z")));
+	Vector3 c_normal = Vector3(extract<double>(normal.attr("x")),
+	                           extract<double>(normal.attr("y")),
+	                           extract<double>(normal.attr("z")));
+	scene->addVertice(c_point, c_normal);
+}
+
+void add_face(object v1, object v2, object v3)
+{
+	scene->addFace(extract<int>(v1),
+	               extract<int>(v2),
+	               extract<int>(v3));
+}
+
+void finish_object()
+{
+	scene->finishObject();
+}
+
+std::string parse_python_exception(){
+    PyObject *type_ptr = NULL, *value_ptr = NULL, *traceback_ptr = NULL;
+    PyErr_Fetch(&type_ptr, &value_ptr, &traceback_ptr);
+    std::string ret("Unfetchable Python error");
+    if(type_ptr != NULL){
+        handle<> h_type(type_ptr);
+        str type_pstr(h_type);
+        extract<std::string> e_type_pstr(type_pstr);
+        if(e_type_pstr.check())
+            ret = e_type_pstr();
+        else
+            ret = "Unknown exception type";
+    }
+    if(value_ptr != NULL){
+        handle<> h_value(value_ptr);
+        str value_pstr(h_value);
+        extract<std::string> e_value_pstr(value_pstr);
+        if (e_value_pstr.check())
+            ret += e_value_pstr();
+        else
+            ret += "Unknown pvalue";
+    }
+    if(traceback_ptr != NULL){
+        handle<> h_tb(traceback_ptr);
+        object tb(import("traceback"));
+        object fmt_tb(tb.attr("format_tb"));
+        object tb_list(fmt_tb(h_tb));
+        object tb_str(str("\n").join(tb_list));
+        extract<std::string> returned(tb_str);
+        if(returned.check())
+            ret += ": " + returned();
+        else
+            ret += std::string(": Unparseable Python traceback");
+    }
+    return ret;
+}
+
+
+
+void addTile(int x_start, int y_start, int width, int height)
+{
+	/*
+	tryClass *tt = new tryClass(x_start, y_start, width, height, data, is_final);
+	delete tt;
+	tt = NULL;
+	*/
+	// std::cout<<"Before calling back!"<<std::endl;
+	PyEval_InitThreads();
+
+    PyGILState_STATE state = PyGILState_Ensure();
+    try{
+        call_method<void>(renderEngine, "update_tile", x_start, y_start, width, height);
+    }catch(boost::python::error_already_set const &){
+        std::string perror_str = parse_python_exception();
+        std::cout << "Error in Python: " << perror_str << std::endl;
+    }
+    PyGILState_Release(state);
+	// std::cout<<"After calling back!"<<std::endl;
+}
+
+char const* new_scene(PyObject *engine)
 {
 	scene = boost::shared_ptr<Scene>(new Scene(1366, 678));
+	renderEngine = engine;
+	Py_INCREF(renderEngine);
+
+	scene->setSendTile(addTile);
 	return "Success!";
 }
 
@@ -35,7 +134,7 @@ void callback()
 char const* render_scene(void)
 {
 	scene->setCallback(callback);
-	scene->renderSceneThreadCamera();
+	scene->renderScene();
 	return "Running!";
 }
 Vector3 get_vect(object v)
@@ -78,13 +177,39 @@ void set_resolution(object width_, object height_)
 	scene->setResolution(width, height);
 }
 
+void set_sample(object sample_)
+{
+	total_sample = extract<int>(sample_);
+	scene->setSample(total_sample);
+}
+
+object get_thread_num()
+{
+	// std::cout<<"in get_thread_num()"<<std::endl;
+	return object(scene->getThreadNum());
+}
+
+void set_grid(object row_n, object col_n)
+{
+	int row_number = extract<int>(row_n);
+	int col_number = extract<int>(col_n);
+	scene->setGrid(row_number, col_number);
+}
+
 BOOST_PYTHON_MODULE(librender)
 {
   def("yay", yay);
+  def("add_object", add_object);
+  def("add_vertice", add_vertice, args("point", "normal"));
+  def("add_face", add_face, args("v1", "v2", "v3"));
+  def("finish_object", finish_object);
+  def("get_thread_num", get_thread_num);
   def("add_objects", add_objects, args("s"), "try docstring");
   def("add_camera", add_camera, args("location", "p1", "p2", "p3", "p4"), "try docstring");
-  def("new_scene",   new_scene);
+  def("new_scene",   new_scene, args("engine"));
   def("render_scene", render_scene);
   def("stop_render", stop_render);
   def("set_resolution", set_resolution, args("width_", "height_"));
+  def("set_sample", set_sample, args("sample_"));
+  def("set_grid", set_grid, args("row_n", "col_n"));
 }
