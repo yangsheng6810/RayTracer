5ee257cb82d20a51460c6d18622ca8f603e8b5e9
Yang Sheng <yangsheng6810@gmail.com>
2013-12-09 20:10:32 +0800

    add png support

--- output.cpp
+++ output.cpp
@@ -1,6 +1,9 @@
 #include <fstream>
+#define BOOST_SYSTEM_NO_LIB
+#define BOOST_FILESYSTEM_NO_LIB
+#include <boost/filesystem.hpp>
 #include "output.h"
-// using namespace boost::gil;
+using namespace boost::gil;
 
 Output::Output(int ww, int hh):
     width(ww), height(hh)
@@ -11,6 +14,8 @@ Output::Output(int ww, int hh):
     // fill_pixels(view(img), black);
 	image = boost::shared_ptr<color_array_type>(new color_array_type(boost::extents[height][width]));
     sample = boost::shared_ptr<number_array_type>(new number_array_type(boost::extents[height][width]));
+	img = rgb8_image_t(width, height);
+	v = boost::gil::view(img);
 	Color c("black");
 	for (int i = 0; i < width; ++i)
 		for(int j = 0; j < height; ++j){
@@ -18,6 +23,12 @@ Output::Output(int ww, int hh):
 	        (*image)[j][i] = 0;
 		}
 
+	namespace fs = boost::filesystem;
+	boost::filesystem::path temp_d = "/tmp/yang-blender";
+	if (boost::filesystem::exists(temp_d)){
+		remove_all(temp_d);
+	}
+	boost::filesystem::create_directory(temp_d);
 }
 
 Output::~Output()
@@ -30,11 +41,13 @@ void Output::setResolution(int width_, int height_)
 	height = height_;
 	image = boost::shared_ptr<color_array_type>(new color_array_type(boost::extents[height][width]));
     sample = boost::shared_ptr<number_array_type>(new number_array_type(boost::extents[height][width]));
+	img = rgb8_image_t(width, height);
+	v = boost::gil::view(img);
 }
 
 void Output::writePic() const
 {
-    boost::mutex::scoped_lock lock(mutex_output);
+    boost::mutex::scoped_lock lock_m(mutex_read);
 	std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
 	ofs << "P6\n" << width << " " << height << "\n255\n";
 	for (unsigned i = 0; i < height; ++i)
@@ -44,12 +57,51 @@ void Output::writePic() const
 		    (unsigned char)((*image)[i][j].b / (*sample)[i][j] * 255);
 	    }
 	ofs.close();
-	// png_write_view("untitled.png", const_view(img));
+	for (unsigned i = 0; i < height; ++i)
+		for(unsigned j = 0; j < width; ++j)
+    		v(j,i) = rgb8_pixel_t(
+			             (unsigned char)((*image)[i][j].r / (*sample)[i][j] * 255),
+			             (unsigned char)((*image)[i][j].g / (*sample)[i][j] * 255),
+            		     (unsigned char)((*image)[i][j].b / (*sample)[i][j] * 255));
+	png_write_view("untitled.png", const_view(img));
+}
+
+std::vector<Color> Output::getTile(int x_start, int y_start, int width, int height) const
+{
+    boost::mutex::scoped_lock lock_r(mutex_read);
+	std::vector<Color> ret;
+	int x_end = x_start + width;
+	int y_end = y_start + height;
+	for (unsigned i = y_start; i < y_end; ++i )
+		for(unsigned j = x_start; j < x_end; ++j )
+			ret.push_back((*image)[i][j]);
+	// std::cout<<"in Output::getTile, before return"<<std::endl;
+	return ret;
+}
+
+void Output::outputTile(int x_start, int y_start, int width, int height) const
+{
+    boost::mutex::scoped_lock lock_r(mutex_read);
+	rgb8_image_t tile(width, height);
+	rgb8_view_t my_view = view(tile);
+	int x_end = x_start + width;
+	int y_end = y_start + height;
+	for (unsigned i = y_start; i < y_end; ++i )
+		for(unsigned j = x_start; j < x_end; ++j )
+			my_view(j - x_start,i - y_start) = rgb8_pixel_t(
+			             (unsigned char)((*image)[i][j].r / (*sample)[i][j] * 255),
+			             (unsigned char)((*image)[i][j].g / (*sample)[i][j] * 255),
+            		     (unsigned char)((*image)[i][j].b / (*sample)[i][j] * 255));
+	std::ostringstream strs;
+	strs<<"/tmp/yang-blender/"<<x_start<<"-"<<y_start<<".png";
+	std::string str = strs.str();
+	png_write_view(str, const_view(tile));
 }
 
 void Output::addColor(Color c, int x, int y)
 {
-    boost::mutex::scoped_lock lock(mutex_modify);
+    boost::mutex::scoped_lock lock_r(mutex_read);
+    boost::mutex::scoped_lock lock_w(mutex_write);
 	c.toRealColor();
 	(*image)[y][x] += c.toRealColor();
 	(*sample)[y][x] += 1;
--- output.h
+++ output.h
@@ -1,14 +1,16 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
-#include "color.h"
 #define png_infopp_NULL (png_infopp)NULL
 #define int_p_NULL (int*)NULL
+#include "color.h"
+#include <vector>
+#include <boost/function.hpp>
 #include <boost/multi_array.hpp>
 #include <boost/smart_ptr.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/thread.hpp>
-// #include <boost/gil/gil_all.hpp>
-// #include <boost/gil/extension/io/png_dynamic_io.hpp>
+#include <boost/gil/gil_all.hpp>
+#include <boost/gil/extension/io/png_dynamic_io.hpp>
 
 typedef boost::multi_array<Color, 2> color_array_type;
 typedef boost::multi_array<int, 2> number_array_type;
@@ -20,16 +22,18 @@ public:
 	~Output();
 	void addColor(Color c, int x, int y);
 	void writePic() const;
+	std::vector<Color> getTile(int x_start, int y_start, int width, int height) const;
+	void outputTile(int x_start, int y_start, int width, int height) const;
 	void setResolution(int width_, int height_);
 	int width, height;
 
 private:
 	boost::shared_ptr<color_array_type> image;
 	boost::shared_ptr<number_array_type> sample;
-	mutable boost::mutex mutex_output;
-	mutable boost::mutex mutex_modify;
-	// boost::gil::rgb8_image_t img;
-	// boost::gil::rgb8_view_t v;
+	mutable boost::mutex mutex_read;
+	mutable boost::mutex mutex_write;
+	boost::gil::rgb8_image_t img;
+	boost::gil::rgb8_view_t v;
 
 };
 
